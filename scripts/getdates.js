1cbc4d7f766273d9c1e3de14cf2723bfa766e969
BARAZAJ <barazajohn52@gmail.com>
2024-04-26 17:23:42 +0300

    js

---
+++ scripts/getdates.js
@@ -0,0 +1,13 @@
+
+function updateFooterContent() {
+    const currentYearElement = document.getElementById('currentYear');
+    const currentYear = new Date().getFullYear();
+    currentYearElement.textContent = `${currentYear}`;
+ 
+    const lastModifiedElement = document.getElementById('lastModified');
+    const lastModifiedDate = document.lastModified;
+    lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+}
+
+
+updateFooterContent();
