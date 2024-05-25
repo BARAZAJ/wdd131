e015c1948681071e1304f55375fdbaed17a3ec39
BARAZAJ <barazajohn52@gmail.com>
2024-05-25 20:34:26 +0300

    new

--- scripts/form.js
+++ scripts/form.js
@@ -36,4 +36,22 @@ const products = [
           productNameSelect.appendChild(option);
       });
   };
+
   
+  function updateFooterContent() {
+    const currentYearElement = document.getElementById('currentYear');
+    const currentYear = new Date().getFullYear();
+    currentYearElement.textContent = `${currentYear}`;
+ 
+    const lastModifiedElement = document.getElementById('lastModified');
+    const lastModifiedDate = document.lastModified;
+    lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+}
+updateFooterContent();
+
+
+
+
+
+
+
