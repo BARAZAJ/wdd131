200d15deb0057707fb847c519cce7f31d1fa909b
BARAZAJ <barazajohn52@gmail.com>
2024-05-25 13:55:52 +0300

    new

--- scripts/form.js
+++ scripts/form.js
@@ -1,16 +1,39 @@
 const products = [
-    { id: 'product1', name: 'Product 1' },
-    { id: 'product2', name: 'Product 2' },
-    { id: 'product3', name: 'Product 3' }
-];
-
-window.onload = () => {
-    const productNameSelect = document.getElementById('productName');
-
-    products.forEach(product => {
-        const option = document.createElement('option');
-        option.value = product.id;
-        option.textContent = product.name;
-        productNameSelect.appendChild(option);
-    });
-};
+    {
+      id: 'fc-1888',
+      name: "flux capacitor",
+      'avg-rating': 4.5
+    },
+    {
+      id: 'fc-2050',
+      name: "power laces",
+      'avg-rating': 4.7
+    },
+    {
+      id: 'fs-1987',
+      name: "time circuits",
+      'avg-rating': 3.5
+    },
+    {
+      id: 'ac-2000',
+      name: "low voltage reactor",
+      'avg-rating': 3.9
+    },
+    {
+      id: 'jj-1969',
+      name: "warp equalizer",
+      'avg-rating': 5.0
+    }
+  ];
+  
+  window.onload = () => {
+      const productNameSelect = document.getElementById('productName');
+  
+      products.forEach(product => {
+          const option = document.createElement('option');
+          option.value = product.id;
+          option.textContent = product.name;
+          productNameSelect.appendChild(option);
+      });
+  };
+  
