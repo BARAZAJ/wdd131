3c65e2144e4e4e3906421af3b2f7668ac463fa4f
BARAZAJ <barazajohn52@gmail.com>
2024-05-25 13:49:13 +0300

    form

---
+++ scripts/form.js
@@ -0,0 +1,16 @@
+const products = [
+    { id: 'product1', name: 'Product 1' },
+    { id: 'product2', name: 'Product 2' },
+    { id: 'product3', name: 'Product 3' }
+];
+
+window.onload = () => {
+    const productNameSelect = document.getElementById('productName');
+
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.id;
+        option.textContent = product.name;
+        productNameSelect.appendChild(option);
+    });
+};
