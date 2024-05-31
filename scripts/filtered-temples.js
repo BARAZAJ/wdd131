ceaab64f4883eecb1ec2cd5ede4981059bd8a811
BARAZAJ <barazajohn52@gmail.com>
2024-05-31 11:18:28 +0300

    new

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -15,61 +15,69 @@ document.addEventListener('DOMContentLoaded', () => {
             location: "Aba, Nigeria",
             dedicated: "2005, August, 7",
             area: 11500,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
-        },
-        {
+            imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+          },
+          {
             templeName: "Manti Utah",
             location: "Manti, Utah, United States",
             dedicated: "1888, May, 21",
             area: 74792,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
-        },
-        {
+            imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+          },
+          {
             templeName: "Payson Utah",
             location: "Payson, Utah, United States",
             dedicated: "2015, June, 7",
             area: 96630,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/800x500/payson-utah-temple-daylight-1416668-wallpaper.jpg"
-        },
-        {
+            imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x250/payson-utah-temple-daylight-1416668-wallpaper.jpg"
+          },
+          {
             templeName: "Yigo Guam",
             location: "Yigo, Guam",
             dedicated: "2020, May, 2",
             area: 6861,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
-        },
-        {
+            imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+          },
+          {
             templeName: "Washington D.C.",
             location: "Kensington, Maryland, United States",
             dedicated: "1974, November, 19",
             area: 156558,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
-        },
-        {
+            imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+          },
+          {
             templeName: "Lima Perú",
             location: "Lima, Perú",
             dedicated: "1986, January, 10",
             area: 9600,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/adelaide-australia/800x450/adelaide-australia-temple-lds-866420-wallpaper.jpg"
-        },
-        {
-            templeName: "Lima Perú",
-            location: "Lima, Perú",
-            dedicated: "1986, January, 10",
-            area: 98000,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/anchorage-alaska/800x500/anchorage-temple-lds-746769-wallpaper.jpg"},
+            imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+          },
+          {
+            templeName: "Mexico City Mexico",
+            location: "Mexico City, Mexico",
+            dedicated: "1983, December, 2",
+            area: 116642,
+            imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+          },
         {
             templeName: "Albuquerque New Mexico",
             location: "Albuquerque, New Mexico",
             dedicated: "2000, March, 4",
             area: 78000,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/800x800/albuquerque-temple-lds-820684-wallpaper.jpg"},
+            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/400x250/albuquerque-temple-lds-137883-wallpaper.jpg"},
         {
             templeName: "Arequipa Perú",
             location: "Arequipa, Perú",
             dedicated: "2017, March, 4",
             area: 120000,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/800x500/2-3c2316607190934fc0265f4107b5013b0cc4b21e.jpeg"}
+            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/400x250/1-55f6c59ce8f9c093a9c689067f8674335de544e2.jpeg"}
     ];
 
     createTempleCard(temples);
